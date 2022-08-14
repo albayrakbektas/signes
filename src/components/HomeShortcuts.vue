577691b7f606d409229b3e3a76643eea43b5f166
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-14 13:02:14 +0300

    Create HomeShortcuts.vue

---
+++ src/components/HomeShortcuts.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div
+      v-for="(item, index) of imgList"
+      :key="index"
+      :class="index.toString()"
+    >
+      <HomeShortcut :img="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import HomeShortcut from "@/components/HomeShortcut";
+export default {
+  name: "HomeShortcuts",
+  components: { HomeShortcut },
+  data() {
+    return {
+      imgList: [
+        {
+          src: require("../assets/shortcuts/project.png"),
+          alt: "projects image",
+          title: "Our projects",
+          subTitle: "Lorem Ipsum",
+          href: "/projects",
+        },
+        {
+          src: require("../assets/shortcuts/document.jpg"),
+          alt: "document image",
+          title: "Our documents",
+          subTitle: "Lorem Ipsum",
+          href: "/documents",
+        },
+        {
+          src: require("../assets/shortcuts/about.png"),
+          alt: "about us image",
+          title: "About us",
+          subTitle: "Lorem Ipsum",
+          href: "/about",
+        },
+        {
+          src: require("../assets/shortcuts/contact.png"),
+          alt: "contact image",
+          title: "Contact us",
+          subTitle: "Lorem Ipsum",
+          href: "/contact",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 2rem 0;
+}
+.\30,
+.\31,
+.\32 {
+  border-right: 1px solid rgba(210, 176, 76, 1);
+}
+</style>
