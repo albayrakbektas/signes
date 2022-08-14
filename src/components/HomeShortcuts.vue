6d1aeb70e888e5b8bf94ef0cd79a063423c2bc50
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-14 13:38:07 +0300

    mobile view

--- src/components/HomeShortcuts.vue
+++ src/components/HomeShortcuts.vue
@@ -53,6 +53,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@media (max-width: 500px) {
+  .container {
+    grid-template-columns: repeat(2, 1fr) !important;
+    grid-template-rows: repeat(2, 1fr);
+  }
+  .\30,
+  .\31,
+  .\32 {
+    border-right: none !important;
+  }
+  .\30,
+  .\31 {
+    border-bottom: 1px solid rgba(210, 176, 76, 1);
+  }
+  .\32,
+  .\33 {
+    align-self: flex-end;
+  }
+}
 .container {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
