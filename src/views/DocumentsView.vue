1b413ddb0e8e19fc3a21fad76167450db33456fc
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-17 20:35:59 +0300

    Update DocumentsView.vue

--- src/views/DocumentsView.vue
+++ src/views/DocumentsView.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="documents"><h1>This is an doc page</h1></div>
+  <div class="documents">
+    <!--    <a href="../assets/pdf/_BektasAlbayrak.pdf"></a>-->
+    <iframe src="_BektasAlbayrak.pdf"></iframe>
+  </div>
 </template>
 
 <script>
 export default {
   name: "DocumentsView",
+  created() {
+    window.scrollTo(0, 0);
+  },
 };
 </script>
 
@@ -12,5 +18,9 @@ export default {
 .documents {
   margin-top: 70px;
   height: calc(100vh - 120px);
+}
+iframe {
+  width: 20vw;
+  height: 20vw;
 }
 </style>
