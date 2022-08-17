24bbcc3c9326f2d3efd27cca793cb83873483041
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-17 17:12:41 +0300

    create product
    create ProductCard and ProductDetailView

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="card-container">
+    <img :src="product.src[0]" :alt="product.alt" />
+    <div class="card-bottom">
+      <div class="title">
+        <h4>
+          <b>{{ product.title }}</b>
+        </h4>
+      </div>
+      <div class="sub-title">
+        <p>{{ product.subTitle }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@media (max-width: 500px) {
+  .card-container,
+  .card-bottom {
+    width: 80vw !important;
+    margin: 0 auto;
+  }
+  img {
+    width: 80vw !important;
+    height: 50vh !important;
+  }
+}
+.card-container {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  align-items: start;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    img {
+      opacity: 0.7;
+      transition: 0.3s;
+    }
+  }
+  margin: 1rem 0;
+}
+img {
+  width: 20vw;
+  height: 20vw;
+}
+.card-container,
+.card-bottom {
+  display: grid;
+  justify-content: center;
+  grid-template-rows: repeat(2, auto);
+  width: 20vw;
+}
+.card-bottom {
+  text-align: left;
+  justify-content: flex-start;
+  align-content: flex-start;
+  height: 100%;
+  padding-bottom: 1.33em;
+}
+.title {
+  h4 {
+    -webkit-margin-after: 0;
+    margin-block-end: 0;
+  }
+}
+h4,
+p {
+  padding: 0 16px;
+}
+.sub-title {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+}
+</style>
---
+++ src/views/ProductDetailView.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="detail-page">
+    <div class="slider-container">
+      <div
+        v-for="(item, index) of product.src"
+        :key="index"
+        class="slider"
+        @mouseover="holdImg"
+        @mouseleave="releaseImg"
+      >
+        <img :src="item" :alt="product.alt" class="slide" />
+      </div>
+      <div class="dots">
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div class="content">
+      <h1>{{ product.title }}</h1>
+      <h4>
+        <span>{{ $t("_DESCRIPTION") }}:</span>
+        {{ product.subTitle }}
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDetailView",
+  data() {
+    return {
+      product: {},
+      slideIndex: 0,
+      isHoldImg: false,
+    };
+  },
+  created() {
+    this.getProduct();
+    // this.productSlider();
+  },
+  mounted() {
+    this.productSlider();
+  },
+  watch: {
+    isHoldImg() {
+      if (!this.isHoldImg) {
+        this.$nextTick(() => {
+          this.productSlider();
+        });
+      }
+    },
+  },
+  methods: {
+    getProduct() {
+      const { id } = this.$route.params;
+      this.product = this.$store.state.products[id];
+    },
+    holdImg() {
+      this.isHoldImg = true;
+    },
+    releaseImg() {
+      this.isHoldImg = false;
+    },
+    productSlider() {
+      if (!this.isHoldImg) {
+        let i;
+        let slides = document.getElementsByClassName("slider");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        this.slideIndex++;
+        if (this.slideIndex > slides.length) {
+          this.slideIndex = 1;
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[this.slideIndex - 1].style.display = "block";
+        dots[this.slideIndex - 1].className += " active";
+        setTimeout(this.productSlider, 2000);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  text-align: left;
+}
+.active-product-image {
+  display: block;
+}
+@media (max-width: 500px) {
+  .detail-page {
+    grid-template-columns: 1fr !important;
+    grid-template-rows: auto auto;
+  }
+  img {
+    width: 100vw !important;
+  }
+}
+.detail-page {
+  margin-top: 70px;
+  min-height: calc(100vh - 120px);
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
+.slider-container,
+.slider,
+.slide {
+  height: 100%;
+}
+.slider-container {
+  position: relative;
+  //height: calc(100vh - 120px);
+}
+.slider {
+  display: none;
+}
+img {
+  //height: calc(100vh - 120px);
+  width: 40vw;
+}
+.content {
+  display: grid;
+  grid-template-rows: repeat(2, auto);
+  justify-content: start;
+  align-content: start;
+  padding: 2px 16px;
+}
+h1 {
+  margin-block-end: 0;
+  text-align: left;
+}
+h4 {
+  font-weight: normal;
+}
+span {
+  font-weight: bold;
+}
+.dots {
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.dot {
+  height: 15px;
+  width: 15px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+.active {
+  background-color: #717171;
+}
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+@keyframes fade {
+  from {
+    opacity: 0.4;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
