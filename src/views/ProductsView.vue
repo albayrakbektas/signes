c8377ecb80544d1511d30dadee57e7e8aa375046
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-08-17 17:13:35 +0300

    Update ProductsView.vue

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,16 +1,46 @@
 <template>
-  <div class="products"><h1>This is an products page</h1></div>
+  <div class="products">
+    <router-link
+      :to="{ name: 'product', params: { id: index } }"
+      v-for="(item, index) of this.$store.state.products"
+      :key="index"
+    >
+      <ProductCard
+        class="card"
+        :product="item"
+        :key="index"
+        @click="seeDetail"
+      />
+    </router-link>
+  </div>
 </template>
 
 <script>
+import ProductCard from "@/components/ProductCard";
 export default {
   name: "ProductsView",
+  components: { ProductCard },
+  data() {
+    return {};
+  },
+  methods: {
+    seeDetail() {},
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .products {
   margin-top: 70px;
-  height: calc(100vh - 120px);
+  min-height: calc(100vh - 120px);
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  justify-content: space-evenly;
+}
+@media (max-width: 500px) {
+  .products {
+    grid-template-columns: 1fr !important;
+    justify-items: center;
+  }
 }
 </style>
